9c9a56477cb443c91b936802c8370e9842d44f37
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-01 20:17:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,54 @@
-# My Code here....
+def map_to_negativize(array)
+  array.map {|ele| ele * -1}
+end 
+
+def map_to_no_change(array)
+  dune = array
+  dune 
+end 
+
+def map_to_double(array)
+  array.map {|ele| ele * 2}
+end 
+
+def map_to_square(array)
+  array.map {|ele| ele * ele}
+end 
+
+def reduce_to_total(array, starting_point = 0)
+  count = starting_point
+  array.each {|ele| count += ele}
+  count 
+end 
+
+# def reduce_to_all_true(array)
+#   if !(array.include?("nil")) && !(array.include?("false"))
+#   return true 
+# else
+#   return false
+# end 
+# end 
+
+def reduce_to_all_true(array)
+  if array.include?("nil") || array.include?("false")
+    return false
+  end 
+  true
+end 
+
+def reduce_to_any_true(array)
+   if array.include?("true") 
+     return true
+   else
+     return false
+   end 
+end 
+
+
+
+
+
+
+
+
+
